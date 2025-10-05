82ab27cb6b165d25b4b23d200000345e9f2eaaf2
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2025-10-06 00:36:39 +1100

    Enable sourcemaps, dedupe react[-dom]

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import analyzer from "vite-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		sourcemap: true,
+	},
+	resolve: {
+		dedupe: ["react", "react-dom"],
+	},
 	plugins: [
 		react({
 			babel: {
